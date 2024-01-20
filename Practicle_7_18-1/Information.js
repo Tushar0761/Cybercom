2acc3a03085b52ef831ab84f83ca695e851ba44c
Tushar0761 <tusharpanchal0761@gmail.com>
2024-01-21 00:42:51 +0530

    CSS updated

---
+++ Practicle_7_18-1/Information.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("th").forEach((head) => {
+  head.classList.add("text-end");
+});
+document.querySelectorAll("td").forEach((data) => {
+  data.classList.add("text-start");
+});
