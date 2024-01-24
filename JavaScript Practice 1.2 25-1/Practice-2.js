c3a632637c0fe02dd30f814f1e7b36a8bd73dab8
Tushar0761 <tusharpanchal0761@gmail.com>
2024-01-25 01:04:21 +0530

    JavaScript Practice 1.2 Done

--- JavaScript Practice 1.2 25-1/Practice-2.js
+++ JavaScript Practice 1.2 25-1/Practice-2.js
@@ -1,23 +1,29 @@
 // 2. Create an object to store information about a product (e.g., name, price, quantity, etc.). Write code to calculate the total cost of a specified quantity of the product.
+
 const steelPipe = {
   name: "Steel Pipe",
   price: 200,
   quantity: 15,
 };
+
 const steelWire = {
   name: "Steel Wire",
   price: 75,
   quantity: 30,
 };
+
 const steelPlate = {
   name: "Steel Plate",
   price: 500,
   quantity: 8,
 };
+
 function calculateCost(obj) {
   return obj.price * obj.quantity;
 }
 
 console.log(calculateCost(steelPipe), "Pipe");
+
 console.log(calculateCost(steelWire), "Wire");
+
 console.log(calculateCost(steelPlate), "Plate");
--- JavaScript Practice 1.2 25-1/Practice-3.js
+++ JavaScript Practice 1.2 25-1/Practice-3.js
@@ -1,15 +1,18 @@
 // 3. Create an array of objects to store information about multiple people or products. Write code to loop through the array and display the information for each object.
+
 const productList = [
   {
     name: "Steel Pipe",
     price: 200,
     quantity: 15,
   },
+
   {
     name: "Steel Wire",
     price: 75,
     quantity: 30,
   },
+
   {
     name: "Steel Plate",
     price: 500,
