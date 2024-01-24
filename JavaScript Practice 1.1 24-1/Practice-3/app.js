f3e216201ed3b54101e7b3ecdd95d3b120352800
Tushar0761 <tusharpanchal0761@gmail.com>
2024-01-24 17:23:10 +0530

    Practice 3 Done

--- JavaScript Practice 1.1 24-1/Practice-3/app.js
+++ JavaScript Practice 1.1 24-1/Practice-3/app.js
@@ -1,16 +1,12 @@
 $("#firstButton").click(() => {
-  console.log(this);
   $("#firstParagraph").text("Red Button is Clicked");
 });
 $("#secondButton").click(() => {
-  console.log(this);
   $("#secondParagraph").text("Blue Button is Clicked");
 });
 $("#thirdButton").click(() => {
-  console.log(this);
   $("#thirdParagraph").text("Green Button is Clicked");
 });
 $("#fourthButton").click(() => {
-  console.log(this);
   $("#fourthParagraph").text("Yellow Button is Clicked");
 });
