c99ff2b7ba89961d40203011bd481e3e37990f36
Tushar0761 <tusharpanchal0761@gmail.com>
2024-01-29 17:51:14 +0530

    index.html added

--- JAVASCRIPT BASIC FUNDAMENTALS/index.js
+++ JAVASCRIPT BASIC FUNDAMENTALS/index.js
@@ -1,28 +1,28 @@
 let list = `document.getElementById()
-- element.setAttribute()
-- element.style
-- element.classList
-- element.className
-- document.createElement()
-- element.appendChild()
-- element.removeChild()
-- element.replaceChild()
-- document.remove()
-- document.createTextNode()
-- document.getElementsByClassName()
-- document.getElementsByTagName()
-- document.querySelector()
-- document.querySelectorAll()
-- element.innerHTML
-- element.textContent
-- element.insertAdjacentHTML()
-- element.getAttribute()`;
+element.setAttribute()
+element.style
+element.classList
+element.className
+document.createElement()
+element.appendChild()
+element.removeChild()
+element.replaceChild()
+document.remove()
+document.createTextNode()
+document.getElementsByClassName()
+document.getElementsByTagName()
+document.querySelector()
+document.querySelectorAll()
+element.innerHTML
+element.textContent
+element.insertAdjacentHTML()
+element.getAttribute()`;
 list = list.split("\n");
 let fs = require("fs");
 
 fs.readdir("./", (err, files) => {
   for (let i = 0; i < files.length; i++) {
-    let a = `<li><a href="./${files[i]}">${list[i]}</a></li>`;
+    let a = `<li class="m-2"><a href="./${files[i]}" target="_blank">${list[i]} Practical 🚀</a></li>`;
 
     console.log(a);
   }
