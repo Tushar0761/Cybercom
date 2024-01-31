38ce0398f5db48099fa765d161cf5ae241c974a2
Tushar0761 <tusharpanchal0761@gmail.com>
2024-01-31 18:00:02 +0530

    Minor Changes

--- 01_30 Product Database using LocalStorage/asset/app.js
+++ 01_30 Product Database using LocalStorage/asset/app.js
@@ -1,206 +1,70 @@
 $(document).ready(() => {
-  addProductCategories();
+  loadProducts();
 });
 
-function addProduct() {
-  let productName = $("#productName").val();
-  let productCategory = $("#productCategory").val();
-  let productPrice = $("#productPrice").val();
-  let prodcutDes = $("#productDes").val();
+function loadProducts() {
+  let ProductList = JSON.parse(localStorage.getItem("ProductList"));
 
-  let isAllValid = validate(productName, productCategory, Number(productPrice));
+  let count = 1;
 
-  if (isAllValid) {
-    addToLocalStorage(productName, productPrice, prodcutDes, productCategory);
+  ProductList.forEach((product) => {
+    let tr = ` <tr>
+        <td>${count}</td>
+        <td>${product.Name}</td>
+        <td>${product.Price}</td>
+        <td>${product.Category}</td>
+        <td>${product.Description}</td>
+        <td>${addButton(count - 1)}</td>
+      </tr>
+    `;
 
-    alert("Saved To Product list...");
-    $("#productDes , #productPrice, #productCategory , #productName").val("");
-  }
+    $("table tbody").append(tr);
+
+    count++;
+  });
 }
 
-function addToLocalStorage(name, price, des, category) {
-  let product = {
-    Name: name,
-    Price: price,
-    Description: des,
-    Category: category,
-  };
+function addButton(count) {
+  return `
+  <btn class="btn btn-success m-1" onclick="editProduct(${count})">Edit</btn>
+  <btn class="btn m-1 btn-danger" onclick="deleteProduct(${count})">Delete</btn>`;
+}
+
+function editProduct(count) {
+  $("#hidden").slideDown();
 
   let ProductList = JSON.parse(localStorage.getItem("ProductList"));
 
-  if (ProductList) {
-    ProductList = [...ProductList, product];
+  let product = ProductList[count];
+
+  document.getElementById("saveBTN").addEventListener("click", function () {
+    if (!ProductList[count]) {
+      alert("This Product is no more present( Deleted in other window.)");
+      location.reload();
+    }
+
+    product.Name = $("#editedName").val() || product.Name;
+
+    product.Description = $("#editedDes").val() || product.Description;
+
+    product.Price = $("#editedPrice").val() || product.Price;
+
+    ProductList[count] = product;
 
     localStorage.setItem("ProductList", JSON.stringify(ProductList));
-  } else {
-    ProductList = [product];
 
-    localStorage.setItem("ProductList", JSON.stringify(ProductList));
-  }
-}
-
-function validate(name, category, price) {
-  let bool = true;
-
-  // Validate Name
-  if (name === "") {
-    $("#productName").addClass("border-danger");
-
-    $("#name-error")
-      .text("Name is Required")
-      .addClass("text-danger")
-      .slideDown();
-
-    bool = false;
-  } else {
-    $("#productName").removeClass("border-danger");
-
-    $("#name-error").slideUp();
-  }
-
-  // Validate Category
-  if (category === "") {
-    $("#productCategory").addClass("border-danger");
-
-    $("#category-error")
-      .text("Category is Required")
-      .addClass("text-danger")
-      .slideDown();
-
-    bool = false;
-  } else {
-    $("#productCategory").removeClass("border-danger");
-
-    $("#category-error").slideUp();
-  }
-
-  // Validate Price
-  if (price === 0) {
-    $("#productPrice").addClass("border-danger");
-
-    $("#price-error")
-      .text("Price is Required")
-      .addClass("text-danger")
-      .slideDown();
-
-    bool = false;
-  } else if (price <= 0) {
-    $("#productPrice").addClass("border-danger");
-
-    $("#price-error")
-      .text("Price Can Not be Negative")
-      .addClass("text-danger")
-      .slideDown();
-    bool = false;
-  } else {
-    $("#productPrice").removeClass("border-danger");
-
-    $("#price-error").slideUp();
-  }
-
-  return bool;
-}
-
-function addProductCategories() {
-  let categories = [
-    "Grain",
-    "Fruit",
-    "Vegetables",
-    "Beans",
-    "Mushrooms & Truffles",
-    "Nuts & Kernels",
-    "Plant Seeds & Bulbs",
-    "Agricultural Growing Media",
-    "Plant & Animal Oil",
-    "Animal Products",
-    "Agriculture & By-product Agents",
-    "Agriculture Products Processing",
-    "Agriculture Projects",
-    "Agricultural Waste",
-    "Agrochemicals",
-    "Charcoal",
-    "Cocoa Beans",
-    "Coffee Beans",
-    "Farm Machinery & Equipment",
-    "Feed",
-    "Flower Pots & Planters",
-    "Fresh Seafood",
-    "Garden Tools",
-    "Grain Products",
-    "Herbal Cigars & Cigarettes",
-    "Organic Produce",
-    "Ornamental Plants",
-    "Other Agriculture Products",
-    "Timber Raw Materials",
-    "Vanilla Beans",
-    "Car Care & Cleaning",
-    "Interior Accessories",
-    "Exterior Accessories",
-    "UTV",
-    "Auto Chassis Parts",
-    "ATV",
-    "Vehicle Tools",
-    "Universal Parts",
-    "Axles",
-    "Cooling System",
-    "Crank Mechanism",
-    "50cc Dirt Bikes",
-    "Auto Electronics",
-    "Transmission",
-    "Automobiles",
-    "Air Intakes",
-    "ATV Parts",
-    "Tricycles",
-    "Body Parts",
-    "Lubrication System",
-    "Motorcycles",
-    "Auto Ignition System",
-    "Fuel System",
-    "Vehicle Equipment",
-    "Motorcycle Accessories",
-    "Auto Steering System",
-    "Motorcycle Parts",
-    "Auto Engine",
-    "Auto Clutch",
-    "Other Auto Parts",
-    "Auto Electrical System",
-    "Automotive & Motorcycle Parts Agent",
-    "Brake System",
-    "Valve Train",
-    "Exhaust System",
-    "Suspension System",
-    "Adhesives & Sealants",
-    "Admixture&Additives",
-    "Basic Organic Chemicals",
-    "Biological Chemical Products",
-    "Catalysts & Chemical Auxiliary Agents",
-    "Chemical Machinery & Equipment",
-    "Petrochemical Products",
-    "Chemical Reagent Products",
-    "Chemical Waste",
-    "Custom Chemical Services",
-    "Daily Chemical Raw Materials",
-    "Flavour & Fragrance",
-    "Inorganic Chemicals",
-    "Lab Supplies",
-    "Non-Explosive Demolition Agents",
-    "Organic Intermediates",
-    "Other Chemicals",
-    "Paints & Coatings",
-    "Pharmaceuticals",
-    "Pigment & Dyestuff",
-    "Polymer",
-    "Surface Treatment Chemicals",
-    "All-In-One PC",
-    "Barebone System",
-    "Blank Media",
-    "Card Reader",
-    "Computer Cables & Connectors",
-    "Computer Cases & Towers",
-  ];
-  categories.forEach((category) => {
-    $("#productCategory").append(
-      `<option value="${category}">${category}</option>`
-    );
+    $("#hidden").slideUp("fast", () => {
+      location.reload();
+    });
   });
+}
+
+function deleteProduct(count) {
+  let ProductList = JSON.parse(localStorage.getItem("ProductList"));
+
+  ProductList.splice(count, 1);
+
+  localStorage.setItem("ProductList", JSON.stringify(ProductList));
+
+  location.reload();
 }
